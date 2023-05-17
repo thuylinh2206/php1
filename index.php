02925cf374defd1644e1daacc6a68191cc0b68ff
Thùy Linh <lduog212@gmail.com>
2023-05-17 13:18:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
- echo "hello" ;
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>
+            BÀI TẬP THỰC HÀNH TRÊN LỚP
+        </h1>
+    </body>
+
+<?php echo "Câu 1:";
+echo "Hello world";
 ?>
+<br/>
+<?php echo "Câu 2:";
+echo strlen("Hello world");
+?>
+<br/>
+</html>
